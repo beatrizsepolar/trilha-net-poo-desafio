dce5725bcb2c944571060168fcde6fe15a59172b
Beatriz Sepolar <sepolarbeatriz@gmail.com>
2023-12-11 23:10:22 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "2199999999", modelo: "Nokia G60 5G", imei: "123456", memoria: 16);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+
+
+Console.WriteLine("Smartfone iPhone:");
+Smartphone iphone = new Iphone(numero: "21666666666", modelo: "iPhone 15 PRO MAX", imei: "8746235", memoria: 128);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
